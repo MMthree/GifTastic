70d4ee4159ef3afd324473a53cfa374a7f26ee90
redyellowblue <42159847+MMthree@users.noreply.github.com>
2018-12-05 19:01:42 -0800

    add js functions for creating starting buttons, getting ajax request and displaying gifs. gif buttons are not working. only search button is working

--- javascript/app.js
+++ javascript/app.js
@@ -1,43 +1,90 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-// var apiKey = "FSyytIrzomRaLQisiLmXTgTFkxeIi72a";
-// https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=5
+    // var apiKey = "FSyytIrzomRaLQisiLmXTgTFkxeIi72a";
+    // https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=5
 
-var btn = $("<button>")
-var gifSearch = "cat";
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=5";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+    var btn = $("<button>");
+    var div = $("<div>");
+    var gifs = ["guitar", "80s", "cat", "dog", "bird"];
+    // var results = response.data;
+    var searchData = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchData + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=10";
+    console.log(gifs);
 
 
-});
+    // Create 5 starting buttons + create new buttons from search inputs
+    function starterBTNs() {
 
+        $("#new-btns").empty();
 
-// Creates 5 starting buttons 
-function starterBTNs() {
-    
-    var starterNames = ["guitar", "80s", "cat", "dog", "bird"];
-     
-        for(var i = 0; i < starterNames.length; i++) {
+        for (var i = 0; i < gifs.length; i++) {
 
-            btn.addClass("btn-primary");
-            btn.attr("data-name", starterNames[i]);
-            btn.text(starterNames[i]);
-            $("#new-btns").append(btn);
+            var starters = $("<button>")
+            starters.addClass("btn-primary");
+            starters.attr("id", "gif-btn");
+            starters.attr("data-name", gifs[i]);
+            starters.text(gifs[i]);
+            $("#new-btns").append(starters);
         }
+    }
 
-}
+    // stores the search terms into the gifs array then adds a new button for the search
+    $("#search").on("click", function () {
+        var textSearch = $("#text-search").val().trim();
+        gifs.push(textSearch)
 
-starterBTNs();
+        // clear the search box after click
+        $("#text-search").val("");
+
+        console.log(gifs);
+        starterBTNs();
+    });
+
+
+
+
+    $(".btn-primary").on("click", function () {
+        var searchData = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchData + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=3";
+
+
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var rating = $("<p>").text("Rated: " + results[i].rating.toUpperCase());
+                rating.addClass("rating");
+                var img = $("<img>").attr("src", results[i].images.fixed_width.url);
+                div.append(rating, img);
+                $("#gifs").prepend(div);
+            }
+            
+
+
+
+
+        });
+    });
 
 
 
 
 
 
-});
+
+
+    // gifs array starting buttons on page load
+    starterBTNs();
+}); // Document.ready
+
+
+
+
