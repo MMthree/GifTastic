00ffe873fae2ffcb481f231de6b54041bee30b28
redyellowblue <42159847+MMthree@users.noreply.github.com>
2018-12-06 18:30:54 -0800

    buttons working

--- javascript/app.js
+++ javascript/app.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
     var btn = $("<button>");
     var div = $("<div>");
     var gifs = ["guitar", "80s", "cat", "dog", "bird"];
-    // var results = response.data;
     var searchData = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchData + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=10";
     console.log(gifs);
@@ -21,32 +20,21 @@ $(document).ready(function () {
         for (var i = 0; i < gifs.length; i++) {
 
             var starters = $("<button>")
-            starters.addClass("btn-primary");
+            starters.addClass("btn-primary btn");
             starters.attr("id", "gif-btn");
             starters.attr("data-name", gifs[i]);
             starters.text(gifs[i]);
             $("#new-btns").append(starters);
         }
-    }
-
-    // stores the search terms into the gifs array then adds a new button for the search
-    $("#search").on("click", function () {
-        var textSearch = $("#text-search").val().trim();
-        gifs.push(textSearch)
-
-        // clear the search box after click
-        $("#text-search").val("");
-
-        console.log(gifs);
-        starterBTNs();
-    });
+    
 
 
 
 
-    $(".btn-primary").on("click", function () {
+    // Displays the gifs and gif rating on button click with ajax request
+    $(".btn").on("click", function () {
         var searchData = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchData + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=3";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchData + "&api_key=FSyytIrzomRaLQisiLmXTgTFkxeIi72a&limit=10";
 
 
 
@@ -64,16 +52,50 @@ $(document).ready(function () {
                 rating.addClass("rating");
                 var img = $("<img>").attr("src", results[i].images.fixed_width.url);
                 div.append(rating, img);
-                $("#gifs").prepend(div);
+                $("#gifs").append(div);
             }
             
 
-
-
-
         });
     });
+    }
 
+    
+    // stores the search terms into the gifs array then adds a new button for the search
+    $("#search").on("click", function () {
+        var textSearch = $("#text-search").val().trim();
+        gifs.push(textSearch)
+
+        textSearch = searchData;
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var rating = $("<p>").text("Rated: " + results[i].rating.toUpperCase());
+                rating.addClass("rating");
+                var img = $("<img>").attr("src", results[i].images.fixed_width.url);
+                div.append(rating, img);
+                $("#gifs").append(div);
+            }
+            
+
+        });
+
+
+
+        // clear the search box after click
+        $("#text-search").val("");
+
+        console.log(gifs);
+        starterBTNs();
+    });
 
 
 
